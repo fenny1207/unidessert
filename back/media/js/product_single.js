df7cdb66b459a896d285cb3a92968cd49b358157
fenny1207 <fenny12077@gmail.com>
2023-07-13 15:12:29 +0800

    上傳後台 要另外再開一個這個back的終端機port是5432

---
+++ back/media/js/product_single.js
@@ -0,0 +1,23 @@
+$(document).ready(function () {
+    $(document).on('click', function (e) {
+        target_id = e.target.id.substr(18)
+        // 抓取第幾個產品傳入後端，依第幾個產品去SELECT後取值[i]再INSERT INTO進資料庫
+        // e.preventDefault();
+        let quantity = 1
+        // console.log(p_name)
+        // console.log(p_price)
+        let data = {
+                "uid": target_id,
+                "quantity": quantity
+            }
+        $.post({
+            url: "http://localhost:5678/product/single",
+            method: "post",
+            contentType: "application/json",
+            data: JSON.stringify(data),
+            success: function(res) {
+                console.log("data")
+            }
+        })
+    })
+})

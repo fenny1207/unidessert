bf87bbccc82ffe0249ce19db35690e6944804826
hsin-yin <hsinyin869171291@gmail.com>
2023-07-17 09:59:56 +0800

    product更改

--- media/js/product_single.js
+++ media/js/product_single.js
@@ -4,19 +4,24 @@ $(document).ready(function () {
         // 抓取第幾個產品傳入後端，依第幾個產品去SELECT後取值[i]再INSERT INTO進資料庫
         // e.preventDefault();
         let quantity = 1
-        // console.log(p_name)
-        // console.log(p_price)
         let data = {
-                "uid": target_id,
-                "quantity": quantity
-            }
+            "pid": target_id,
+            "quantity": quantity
+        }
         $.post({
             url: "http://localhost:5678/product/single",
             method: "post",
             contentType: "application/json",
             data: JSON.stringify(data),
-            success: function(res) {
-                console.log("data")
+            success: function (res) {
+                if(res) {
+                    console.log('表示未登入');
+                    return alert('請先登入再進行此操作');
+                }
+                alert('成功加入購物車')
+            },
+            error: function (err) {
+                alert("發生錯誤");
             }
         })
     })

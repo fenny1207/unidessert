14e7d4c025ba8035d63cb780618aa7e86789859b
ZESYuan <b10742061@gemail.yuntech.edu.tw>
2023-07-23 12:48:47 +0800

    更新ajax

--- media/js/script.js
+++ media/js/script.js
@@ -33,25 +33,54 @@ $(window).scroll(function () {
 
 //購物車
 $(document).ready(function () {
+  // 取得所有「加入購物車」按鈕
   let addtocart = document.querySelectorAll('.addtocart');
 
   for (var i = 0; i < addtocart.length; i++) {
-      addtocart[i].addEventListener('click', function (e) {
-          // console.log(e)
-          var button = $(this);
-          var cart = $("#cart");
-          var cartTotal = cart.attr("data-totalitems");
-          var newCartTotal = parseInt(cartTotal) + 1;
+    // 監聽每個「加入購物車」按鈕的點擊事件
+    addtocart[i].addEventListener('click', function (e) {
+      // console.log(e);
+      var button = $(this); // 取得點擊的按鈕
+      var cart = $("#cart"); // 取得購物車的元素
+      var cartTotal = cart.attr("data-totalitems"); // 取得購物車中商品的總數
+      var newCartTotal = parseInt(cartTotal) + 1; // 新的購物車商品總數（加 1）
 
-          button.addClass("sendtocart");
-          setTimeout(function () {
-              button.removeClass("sendtocart");
-              cart.addClass("shake").attr("data-totalitems", newCartTotal);
-              setTimeout(function () {
-                  cart.removeClass("shake");
-              }, 500);
-          }, 300);
-      }, false);
+      button.addClass("sendtocart"); // 加入送出購物車的動畫效果
+      setTimeout(function () {
+        button.removeClass("sendtocart"); // 移除送出購物車的動畫效果
+        cart.addClass("shake").attr("data-totalitems", newCartTotal); // 購物車抖動效果並更新商品總數
+        setTimeout(function () {
+          cart.removeClass("shake"); // 停止購物車抖動效果
+        }, 500);
+      }, 300);
+
+      // 從點擊的按鈕的 data 屬性或其他來源取得產品資訊
+      var productId = button.attr("data-productid"); // 產品 ID
+      var price = parseFloat(button.attr("data-price")); // 產品價格
+      var quantity = 1; // 你可以在這裡設定數量，或從其他來源獲取數量（如果需要的話）
+
+      // 建立產品資料物件，準備傳送給伺服器
+      var productData = {
+        productId: productId, // 產品 ID
+        price: price, // 產品價格
+        quantity: quantity, // 產品數量
+      };
+
+      // 使用 AJAX 將產品資料傳送給伺服器
+      $.ajax({
+        type: "POST",
+        url: "/addToCart", // 伺服器端的處理端點
+        data: productData, // 要傳送的產品資料
+        success: function (response) {
+
+          console.log(response.message); // 返回的成功訊息
+        },
+        error: function (error) {
+          // 處理錯誤
+          console.error("加入購物車時發生錯誤:", error);
+        },
+      });
+    }, false);
   }
 });
 

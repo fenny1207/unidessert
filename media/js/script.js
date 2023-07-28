e3b71140e9d3d8b266158396bb8a4516850df899
hsin-yin <hsinyin869171291@gmail.com>
2023-07-28 15:07:22 +0800

    script.js修bug

--- media/js/script.js
+++ media/js/script.js
@@ -53,125 +53,71 @@ $(window).scroll(function () {
 //     }, false);
 //   }
 // });
+// ===================================
 
-// 改寫上面的購物車
-// 確認使用者登入後才可以加商品到購物車，購物車上的數字才可以加 1
-// 如果沒有登入，購物車上的數字會保持為 0
-// $(document).ready(function () {
-//   // 取得所有「加入購物車」按鈕
-//   let addtocart = document.querySelectorAll('.addtocart');
+$(document).ready(function () {
+  // 導覽列購物車 icon 數字跟著購物車數量變化
+  function updateCartIconCount(count) {
+    $('#cartNumber').text(count);
+  }
 
-//   for (var i = 0; i < addtocart.length; i++) {
-//     // 監聽每個「加入購物車」按鈕的點擊事件
-//     addtocart[i].addEventListener('click', function (e) {
-//       // console.log(e);
-//       var button = $(this); // 取得點擊的按鈕
-//       var cart = $("#cart"); // 取得購物車的元素
-//       var cartTotal = cart.attr("data-totalitems"); // 取得購物車中商品的總數
-//       var newCartTotal = parseInt(cartTotal) + 1; // 新的購物車商品總數（加 1）
+  // 從後端獲取購物車數量並更新購物車圖示數字
+  function getAndSetCartCount() {
+    $.ajax({
+      url: '/cart/count',
+      type: 'GET',
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        var cartCount = data.cartCount;
+        updateCartIconCount(cartCount);
+      },
+      error: function (error) {
+        console.error('Error fetching cart count:', error);
+      },
+    });
+  }
 
-//       button.addClass("sendtocart"); // 加入送出購物車的動畫效果
-//       setTimeout(function () {
-//         button.removeClass("sendtocart"); // 移除送出購物車的動畫效果
-//         cart.addClass("shake").attr("data-totalitems", newCartTotal); // 購物車抖動效果並更新商品總數
-//         setTimeout(function () {
-//           cart.removeClass("shake"); // 停止購物車抖動效果
-//         }, 500);
-//       }, 300);
+  // 加入商品到購物車
+  function addToCart() {
+    $.ajax({
+      url: '/cart/add',
+      type: 'POST',
+      data: {},
+      dataType: 'json',
+      cache: false,
+      success: function () {
+        getAndSetCartCount(); // 回傳成功時更新購物車數字
+      },
+      error: function (error) {
+        console.error('Error adding to cart:', error);
+      },
+    });
+  }
 
-//       // // 從點擊的按鈕的 data 屬性或其他來源取得產品資訊
-//       // var productId = button.attr("data-productid"); // 產品 ID
-//       // var price = parseFloat(button.attr("data-price")); // 產品價格
-//       // var quantity = 1; // 你可以在這裡設定數量，或從其他來源獲取數量（如果需要的話）
+  $('.addtocart').on('click', function (e) {
+    addToCart();
+  });
 
-//       // // 建立產品資料物件，準備傳送給伺服器
-//       // var productData = {
-//       //   productId: productId, // 產品 ID
-//       //   price: price, // 產品價格
-//       //   quantity: quantity, // 產品數量
-//       // };
+  // 初始化時獲取購物車數量
+  getAndSetCartCount();
 
-//       // 使用 AJAX 將產品資料傳送給伺服器
-//       // $.ajax({
-//       //   type: "POST",
-//       //   url: "/addToCart", // 伺服器端的處理端點
-//       //   data: productData, // 要傳送的產品資料
-//       //   success: function (response) {
-
-//       //     console.log(response.message); // 返回的成功訊息
-//       //   },
-//       //   error: function (error) {
-//       //     // 處理錯誤
-//       //     console.error("加入購物車時發生錯誤:", error);
-//       //   },
-//       // });
-//     }, false);
-//   }
-// });
-// =============================================
-// $(document).ready(function () {
-//   // 檢查使用者登入狀態的 AJAX 請求
-//   $.ajax({
-//     type: 'GET',
-//     url: '/user/checkLogin',
-//     dataType: 'json',
-//     success: function (data) {
-//       if (data.loggedIn) {
-//         // 使用者已登入，隱藏登入按鈕
-//         $('.signin').hide();
-//       } else {
-//         // 使用者尚未登入，隱藏登出按鈕
-//         $('.dropdown .signout').hide();
-//       }
-//     },
-//     error: function (err) {
-//       console.error('無法檢查使用者登入狀態:', err);
-//     },
-//   });
-// });
-// ========================================
-// 導覽列購物車 icon 數字跟著購物車數量變化
-function updateCartIconCount(count) {
-  $('#cartNumber').text(count);
-}
-
-// 從後端獲取購物車數量
-function getCartCount() {
+  // 檢查使用者登入狀態的 AJAX 請求
   $.ajax({
-    url: '/cart/count',
     type: 'GET',
+    url: '/user/checkLogin',
     dataType: 'json',
-    cache: false,
     success: function (data) {
-      var cartCount = data.cartCount;
-      updateCartIconCount(cartCount);
+      if (data.loggedIn) {
+        // 使用者已登入，隱藏登入按鈕
+        $('.signin').hide();
+      } else {
+        // 使用者尚未登入，隱藏登出按鈕
+        $('.dropdown .signout').hide();
+      }
     },
-    error: function (error) {
-      console.error('Error fetching cart count:', error);
+    error: function (err) {
+      console.error('無法檢查使用者登入狀態:', err);
     },
   });
-}
-
-// 加入商品到購物車
-function addToCart() {
-  $.ajax({
-    url: '/cart/add',
-    type: 'POST',
-    data: {},
-    dataType: 'json',
-    cache: false,
-    success: function (data) {
-      getCartCount()
-    },
-    error: function (error) {
-      console.error('Error adding to cart:', error);
-    },
-  });
-}
-$('.addtocart').on('click', function (e) {
-  addToCart()
-  // getCartCount()
 });
-
-// 初始化時獲取購物車數量
-getCartCount();
